8fb16be8ca33962fe919410770e23e23d6768892
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-04-24 17:48:36 -0300

    adding js file

---
+++ main.js
@@ -0,0 +1,20 @@
+var countSheep = function (num){
+  // Setup a string to tabulate the total number of sheep
+  let newString = "";
+  
+  // for loop that iterates through adding each instance to tabulation string
+  for (let i = 1; i <= num; i++) {
+    newString += `${i} sheep...`;
+  }
+  // return the finished string
+  return newString;
+}
+
+
+ -------- __@      __@       __@       __@      __~@
+ ----- _`\<,_    _`\<,_    _`\<,_     _`\<,_    _`\<,_
+ ---- (*)/ (*)  (*)/ (*)  (*)/ (*)  (*)/ (*)  (*)/ (*)
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+------------------------------------------------
+ 
